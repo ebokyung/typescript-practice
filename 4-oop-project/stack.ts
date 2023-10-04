65641de9b187d908f244d36637e88094462f97bd
ebokyung <dlqhrud303@naver.com>
2023-10-04 21:41:28 +0900

    객체지향 프로그래밍 챌린지 풀이

--- 4-oop-project/stack.ts
+++ 4-oop-project/stack.ts
@@ -9,28 +9,36 @@ class OneNode {
     constructor(private pre: any, private value: string){}
 }
 
-class Stack implements Stack {
-    // private size: number = 0;
+class StackImpl implements Stack {
+    private _size: number = 0;
+    get size() {
+        return this._size;
+    }
     private head: any = null;
     constructor(){}
     push(value: string): void{
+        // 스택에 더 넣을 수 있는지 확인
         const newNode = new OneNode(this.head, value)
         this.head = newNode;
-        // this.size++;
+        this._size++;
     }
     pop(): string {
+        // 스택이 비었는지 확인
         const curNode = this.head;
         this.head = curNode.pre;
-        // this.size--;
+        this._size--;
         return curNode.value;
     }
 }
 
-const stack = new Stack();
+const stack = new StackImpl();
+console.log(stack.size)
 stack.push('1')
 stack.push('2')
+console.log(stack.size)
 stack.push('3')
 console.log(stack.pop())
 console.log(stack.pop())
+console.log(stack.size)
 console.log(stack.pop())
 }
