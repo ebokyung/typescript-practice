5dce4a395b996f4e1984c3c82575528432774ebf
ebokyung <dlqhrud303@naver.com>
2023-10-19 16:52:37 +0900

    motion-ellie : video component 만들기 (1)

---
+++ motion-ellie/src/components/page/item/video.ts
@@ -0,0 +1,15 @@
+import { BaseComponent } from './../../component.js';
+export class VideoComponent extends BaseComponent<HTMLElement> {
+    constructor(title: string, url: string) {
+    super(`<section class="video">
+            <div class="video__player"><iframe class="video__iframe"></iframe></div>
+            <h3 class="video__title"></h3>
+        </section>`);
+
+    const iframe = this.element.querySelector('.video__iframe')! as HTMLIFrameElement;
+    iframe.src = url;
+
+    const titleElement = this.element.querySelector('.video__title')! as HTMLHeadingElement;
+    titleElement.textContent = title;
+    }
+}
