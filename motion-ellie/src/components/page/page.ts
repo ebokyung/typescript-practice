8098ff878c605c6311b7896df17edc6669b75f61
ebokyung <dlqhrud303@naver.com>
2023-10-23 21:18:25 +0900

    motion-ellie : page item component 만들기

--- motion-ellie/src/components/page/page.ts
+++ motion-ellie/src/components/page/page.ts
@@ -1,7 +1,29 @@
 import { BaseComponent } from './../component.js';
 
+export class PageItemComponent extends BaseComponent<HTMLUListElement> {
+    constructor() {
+        super(`<li class="page-item">
+            <section class="page-item__body"></section>
+            <div class="page-item__controls">
+                <button class="close">&times;</button>
+            </div>
+        </li>`);
+    }
+
+    addChild(child: Component){
+        const container = this.element.querySelector('.page-item__body')! as HTMLElement;
+        child.attachTo(container);
+    }
+}
+
 export class PageComponent extends BaseComponent<HTMLUListElement> {
     constructor() {
-        super('<ul class="page">This is PageComponenet!</ul>');
+        super('<ul class="page"></ul>');
+    }
+
+    addChild(section: Component){
+        const item = new PageItemComponent();
+        item.addChild(section);
+        item.attachTo(this.element, 'beforeend');
     }
 }
